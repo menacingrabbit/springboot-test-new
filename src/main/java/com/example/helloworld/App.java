96bc11f672d58efa23e1b4583493eb61edc269d5
Benjamin <sigmatix@web.de>
2025-07-18 19:43:58 +0200

    Change to maven clean install app

--- src/main/java/com/example/helloworld/App.java
+++ src/main/java/com/example/helloworld/App.java
@@ -6,13 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-//@RestController
+@RestController
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        SpringApplication.run(App.class, args);
     }
-    //@GetMapping("/")
-//    public String hello() {
-//        return "Hello, World!";
-//    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
 }
